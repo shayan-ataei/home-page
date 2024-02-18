3d28fa41843026fd4c8b71c6efd1e3b3f15d2bb4
spotifyacc <53423678+shayan-ataei@users.noreply.github.com>
2024-02-18 14:21:53 +0330

    optimize step 2: (add base.html-templates, general.css), add github icon

--- home-page/settings_dev.py
+++ home-page/settings_dev.py
@@ -56,8 +56,8 @@ ROOT_URLCONF = "home-page.dev_urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
-        "APP_DIRS": True,
+        'DIRS': [ BASE_DIR / 'templates' ],
+        'APP_DIRS': True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "statics"
+    BASE_DIR / 'static'
 ]
 
 # Default primary key field type
